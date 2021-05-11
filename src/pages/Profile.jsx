532afa3697b2fbd22ac16517eb5be52a82d8c10c
unknown <bassey.rhema@yahoo.com>
2021-05-11 23:38:22 +0100

    styling buttons, adding more input fields in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,34 +1,61 @@
 import React, { useState } from "react";
+import { forEachChild } from "typescript";
 const username = "Rhema";
-var prevEdit = "";
+var prevEdit = {};
+let ageList = []
+
 
 // set an array/variable to push edits into, so you can revert to the last one when 'cancel' is pressed
 export default function Profile() {
+  // 
+  for(var i=13; i<101; i++){
+    ageList.push(i)
+  }
+  ageList.push("100+")
+
+  
   const [editMode, setEditMode] = useState(false);
-  const [aboutMe, setAboutMe] = useState("");
+  const [details, setDetails] = useState({
+    aboutMe:"",
+    emailAddress:"",
+    twitterHandle: "",
+    location:"",
+    gender:"",
+    age:""
+  });
 
 
-  function editAboutMe(event) {
+  function editDetails(event) {
     setEditMode(true);
     event.preventDefault();
   }
 
   function handleChange(e) {
-    const { value } = e.target;
-    setAboutMe(value);
+    const { name, value } = e.target;
+    setDetails((prevDetails) => {
+      return{
+        ...prevDetails,
+        [name]: value
+      }
+    });
   }
 
   function updateEdit(e) {
     setEditMode(false);
 
     e.preventDefault();
-    prevEdit = aboutMe;
+    prevEdit = details;
   }
 
   function cancleEdit(e) {
-    setAboutMe(prevEdit);
+    
+    setDetails(prevEdit);
     setEditMode(false);
     e.preventDefault();
+  }
+
+  function generateList(){
+    return(<option>test</option>)
   }
 
   return (
@@ -39,22 +66,77 @@ export default function Profile() {
         {editMode ? (
           <div>
             <textarea
+              name="aboutMe"
               placeholder="About Me..."
               onChange={handleChange}
-              value={aboutMe}
+              value={details.aboutMe}
             />
             <p>
-              <button type="submit" onClick={updateEdit}>
+
+            <form>
+              <label>Email: </label>
+              <input name="emailAddress" value={details.emailAddress} onChange={handleChange}/>
+            </form>
+            <form>
+              <label>Twitter: </label>
+              <input name="twitterHandle" value={details.twitterHandle} onChange={handleChange}/>
+            </form>
+            <form>
+              <label>Location: </label>
+              <input name="location" value={details.location} onChange={handleChange}/>
+            </form>
+            <form>
+            <label>Gender: </label>
+            <select name="gender" onChange={handleChange}>
+                  {details.gender ==="" ? <option value="-" selected>--Gender--</option>: <option value="-" selected>{details.gender}</option>}
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Non-Binary">Non Binary</option>
+                  <option value="Trans"> Trans </option>
+                  <option value="Prefer not to say">Prefer not to say</option>
+                </select>
+            </form>
+            <form>
+            <label>Age: </label>
+            <select name="age" onChange={handleChange}>
+               {details.age ==="" ? <option value="-" selected>--Age--</option>: <option value="-" selected>{details.age}</option>}
+
+                <option value="Prefer not to say"> Prefer not to say</option>
+               {ageList.map((item) => {
+                 return(<option value={item}>{item}</option>)
+               })}
+            </select>
+            </form>
+            
+              <button
+                type="submit"
+                onClick={updateEdit}
+                class="btn btn-primary"
+              >
                 OK
               </button>
-              <button onClick={cancleEdit}> Cancel </button>
+
+              <button onClick={cancleEdit} class="btn btn-outline-dark">
+                Cancel
+              </button>
             </p>
           </div>
         ) : (
           <div>
-            <p>{aboutMe}</p>
+            <p>{details.aboutMe}</p>
+            <p>Email: {details.emailAddress}</p>
+            <p>Twitter: {details.twitterHandle}</p>
+            <p>Location: {details.location}</p>
+            <p>Gender: {details.gender}</p>
+            <p>Age: {details.age}</p>
             <p>
-              <button onClick={editAboutMe}>Edit</button>{" "}
+              <button
+                onClick={editDetails}
+                type="button"
+                class="btn btn-primary"
+              >
+                Edit
+              </button>{" "}
             </p>
           </div>
         )}
